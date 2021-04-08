e18803154e2ee284c9c1a1bd151a6db04eb6bf6d
Alex93pa <Ale.pug2@gmail.com>
2021-04-08 15:47:28 +0200

    sistemare decimali

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-var kmPercorsi = prompt("Quanti km vuoi percorrere?");
-console.log(kmPercorsi);
+var kmPercorsi = parseInt (prompt("Quanti km vuoi percorrere?"));
+console.log(kmPercorsi + " " + "km");
 
-var anni = prompt("Quanti anni hai?")
-console.log(anni);
+var anni = parseInt ( prompt("Quanti anni hai?"));
+console.log(anni + " " + "anni");
+
+// var prezzo = kmPercorsi * 0.21
+// console.log(prezzo + "€")
+
+if ( anni < 18){
+    var prezzo = kmPercorsi * 0.21
+    var prezzoFinale = prezzo * 0.80
+    console.log("prezzo scontato under 18" + " " + prezzoFinale + "€");
+}
+
+if (anni > 18){
+    var prezzo = kmPercorsi * 0.21
+    console.log(prezzo + "€")
+}
+
+
+if ( anni > 65){
+    var prezzo = kmPercorsi * 0.21
+    var prezzoFinale = prezzo * 0.60
+    console.log("prezzo scontato over 65" + " " + prezzoFinale + "€");
+}
