5304274bd7e128b1a73291866b0ebf672357aabe
Alex93pa <Ale.pug2@gmail.com>
2021-04-08 18:07:48 +0200

    fine

--- script.js
+++ script.js
@@ -12,25 +12,24 @@ if (Number.isNaN(kmPercorsi) || Number.isNaN(anni)) {
 
 }else {
     
-}
-
         
-if ( anni < 18){
+    if ( anni < 18){
     var prezzo = kmPercorsi * 0.21
     var prezzoFinale = prezzo * 0.80
     console.log("prezzo scontato under 18 =" + " " + prezzoFinale.toFixed( 2 ) + "€");
     document.getElementById('costo').innerHTML = "prezzo scontato under 18 =" + " " + prezzoFinale.toFixed( 2 ) + "€";
-}
+    }
 
-else if ( anni > 65){
+    else if ( anni > 65){
     var prezzo = kmPercorsi * 0.21
     var prezzoFinale = prezzo * 0.60
     console.log("prezzo scontato over 65 =" + " " + prezzoFinale.toFixed( 2 ) + "€");
     document.getElementById('costo').innerHTML = "prezzo scontato over 65 =" + " " + prezzoFinale.toFixed( 2 ) + "€";
-}
+    }
 
-else {
+    else {
     var prezzo = kmPercorsi * 0.21
     console.log(prezzo.toFixed( 2 ) + "€")
     document.getElementById('costo').innerHTML = prezzo.toFixed( 2 ) + " " + "€";
-} 
+    } 
+}
