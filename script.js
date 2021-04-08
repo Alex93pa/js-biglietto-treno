400478e21f83cf98f7f6633661f75ec0ab88f64f
Alex93pa <Ale.pug2@gmail.com>
2021-04-08 22:42:39 +0200

    fine - migliorato js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var kmPercorsi = parseInt (prompt("Quanti km vuoi percorrere?"));
 console.log(kmPercorsi + " " + "km");
 
-
 var anni = parseInt ( prompt("Quanti anni hai?"));
 console.log(anni + " " + "anni");
 
@@ -11,25 +10,18 @@ if (Number.isNaN(kmPercorsi) || Number.isNaN(anni)) {
     document.getElementById('error').innerHTML = "errore inserimento dei dati";
 
 }else {
-    
-        
+    var prezzo = kmPercorsi * 0.21
+            
     if ( anni < 18){
-    var prezzo = kmPercorsi * 0.21
-    var prezzoFinale = prezzo * 0.80
-    console.log("prezzo scontato under 18 =" + " " + prezzoFinale.toFixed( 2 ) + "€");
-    document.getElementById('costo').innerHTML = "prezzo scontato under 18 =" + " " + prezzoFinale.toFixed( 2 ) + "€";
-    }
-
-    else if ( anni > 65){
-    var prezzo = kmPercorsi * 0.21
-    var prezzoFinale = prezzo * 0.60
-    console.log("prezzo scontato over 65 =" + " " + prezzoFinale.toFixed( 2 ) + "€");
-    document.getElementById('costo').innerHTML = "prezzo scontato over 65 =" + " " + prezzoFinale.toFixed( 2 ) + "€";
-    }
-
-    else {
-    var prezzo = kmPercorsi * 0.21
-    console.log(prezzo.toFixed( 2 ) + "€")
-    document.getElementById('costo').innerHTML = prezzo.toFixed( 2 ) + " " + "€";
+        var prezzoFinale = prezzo * 0.80
+        console.log("prezzo scontato under 18 =" + " " + prezzoFinale.toFixed( 2 ) + "€");
+        document.getElementById('costo').innerHTML = "prezzo scontato under 18 =" + " " + prezzoFinale.toFixed( 2 ) + "€";
+    } else if ( anni > 65) {
+        var prezzoFinale = prezzo * 0.60
+        console.log("prezzo scontato over 65 =" + " " + prezzoFinale.toFixed( 2 ) + "€");
+        document.getElementById('costo').innerHTML = "prezzo scontato over 65 =" + " " + prezzoFinale.toFixed( 2 ) + "€";
+    } else {
+        console.log(prezzo.toFixed( 2 ) + "€")
+        document.getElementById('costo').innerHTML = prezzo.toFixed( 2 ) + " " + "€";
     } 
 }
