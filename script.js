93114e8792d7adfa416cb0d4474d82f7d074de4b
Alex93pa <Ale.pug2@gmail.com>
2021-04-08 17:56:42 +0200

    fine - sistemare errore dati

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 var kmPercorsi = parseInt (prompt("Quanti km vuoi percorrere?"));
 console.log(kmPercorsi + " " + "km");
 
-if (Number.isNaN(kmPercorsi)) {
-    alert("non valido")
-}
 
 var anni = parseInt ( prompt("Quanti anni hai?"));
 console.log(anni + " " + "anni");
 
-if (Number.isNaN(anni)) {
+if (Number.isNaN(kmPercorsi) || Number.isNaN(anni)) {
+    
     alert("non valido")
+    document.getElementById('error').innerHTML = "errore inserimento dei dati";
+
 }
 
-
-
+        
 if ( anni < 18){
     var prezzo = kmPercorsi * 0.21
     var prezzoFinale = prezzo * 0.80
@@ -21,16 +20,15 @@ if ( anni < 18){
     document.getElementById('costo').innerHTML = "prezzo scontato under 18 =" + " " + prezzoFinale.toFixed( 2 ) + "€";
 }
 
-if (anni > 18){
-    var prezzo = kmPercorsi * 0.21
-    console.log(prezzo.toFixed( 2 ) + "€")
-    document.getElementById('costo').innerHTML = prezzo.toFixed( 2 ) + " " + "€";
-}
-
-
-if ( anni > 65){
+else if ( anni > 65){
     var prezzo = kmPercorsi * 0.21
     var prezzoFinale = prezzo * 0.60
     console.log("prezzo scontato over 65 =" + " " + prezzoFinale.toFixed( 2 ) + "€");
     document.getElementById('costo').innerHTML = "prezzo scontato over 65 =" + " " + prezzoFinale.toFixed( 2 ) + "€";
 }
+
+else {
+    var prezzo = kmPercorsi * 0.21
+    console.log(prezzo.toFixed( 2 ) + "€")
+    document.getElementById('costo').innerHTML = prezzo.toFixed( 2 ) + " " + "€";
+} 
